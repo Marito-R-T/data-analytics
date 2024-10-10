0c6edb142e1952557d9243d8534312aa66f3946d
Mario Ramírez Tobar <mariomoises20008@gmail.com>
2024-10-10 00:22:57 -0600

    First script to export csv querys

---
+++ Scripts/DVDRental.sql
@@ -0,0 +1,29 @@
+SELECT * FROM actor;
+
+SELECT * FROM address;
+
+SELECT * FROM category;
+
+SELECT * FROM city;
+
+SELECT * FROM country;
+
+SELECT * FROM customer;
+
+SELECT * FROM film;
+
+SELECT * FROM film_actor;
+
+SELECT * FROM film_category;
+
+SELECT * FROM inventory;
+
+SELECT * FROM language;
+
+SELECT * FROM payment;
+
+SELECT * FROM rental;
+
+SELECT * FROM staff;
+
+SELECT * FROM store;
